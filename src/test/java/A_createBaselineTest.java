0d60d212cd442b95a2728e15aa165d42d4904f9c
John Pratt <jpratt2@gmail.com>
2021-04-09 15:06:00 -0700

    minor comment

--- src/test/java/A_createBaselineTest.java
+++ src/test/java/A_createBaselineTest.java
@@ -6,10 +6,11 @@ public class A_createBaselineTest {
 
     @Test
     public void createBaselineImages() throws InterruptedException {
+        //create a full-page image of the URLs in URLs.list
+
         //set-up
         System.setProperty("webdriver.gecko.driver", "C:\\webdrivers\\geckodriver.exe");
 
-        //create a full-page image of the URLs in URLs.list
         for(int i = 0; i < URLs.list.length; i++){
             FirefoxDriver driver = new FirefoxDriver();//start a new, fresh browser for each web page
             driver.manage().window().maximize();
