71849cdcf1f244e67e1008399f0634ff062b2fdf
John Pratt <jpratt2@gmail.com>
2021-04-09 15:04:37 -0700

    open each URL in a fresh, new browser

--- src/test/java/A_createBaselineTest.java
+++ src/test/java/A_createBaselineTest.java
@@ -1,16 +1,24 @@
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class A_createBaselineTest extends Setup {
+public class A_createBaselineTest {
 
     @Test
     public void createBaselineImages() throws InterruptedException {
+        //set-up
+        System.setProperty("webdriver.gecko.driver", "C:\\webdrivers\\geckodriver.exe");
+
         //create a full-page image of the URLs in URLs.list
         for(int i = 0; i < URLs.list.length; i++){
+            FirefoxDriver driver = new FirefoxDriver();//start a new, fresh browser for each web page
+            driver.manage().window().maximize();
             String URL = URLs.list[i];
             driver.get(URL);
             Thread.sleep(3000); //wait for the page to fully load
             Screenshot.fullpage(driver);
+            driver.quit();
+
         }
     }
 
--- src/test/java/B_compareImagesTest.java
+++ src/test/java/B_compareImagesTest.java
@@ -1,16 +1,24 @@
 import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-public class B_compareImagesTest extends Setup{
+public class B_compareImagesTest {
     @Test
     public void testAgainstBaseline() throws InterruptedException {
         //create a full-page image of the URLs in the list, but this time in the "observed" folder
 
+        //set-up
+        System.setProperty("webdriver.gecko.driver", "C:\\webdrivers\\geckodriver.exe");
+
         for (int i = 0; i < URLs.list.length; i++) {
+            FirefoxDriver driver = new FirefoxDriver();//start a new, fresh browser for each web page
+            driver.manage().window().maximize();
             String URL = URLs.list[i];
             driver.get(URL);
             Thread.sleep(3000);//wait for the page to fully load
             Screenshot.compareImage(driver);
+            driver.quit();
+
         }
     }
 }
